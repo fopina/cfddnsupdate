c473015d4fef6c49c07e890a6f798b8556e52579
Filipe Pina <636320+fopina@users.noreply.github.com>
2024-03-26 00:34:39 +0000

    v1 (#1)

---
+++ README.md
@@ -0,0 +1,72 @@
+# cfddnsupdate
+
+A Cloudflare worker to expose a `/nic/update`-compatible endpoint to update your DNS records in Cloudflare.
+
+## Why
+
+[noip](https://noip.com/) has been the king of free DynDNS for decades, directly supported in most router stock firmware. But their free plan requires a to [push a button](https://lostpedia.fandom.com/wiki/Pushing_the_button) once a month, solving a captcha, to keep the hostname active.
+
+I already use [homeassistant cloudflare](https://www.home-assistant.io/integrations/cloudflare/) integration to keep a DNS record up to date. But, in another network, I don't have any device other than the router, a [Thompson TG789](https://openwrt.org/inbox/toh/technicolor/tg789vac_v2), restricted to stock firmware.  
+This firmware allows us to set a `custom` server for DynDNS but it will call it always with the payload `/nic/update?system=custom&hostname=UI_SETTING&myip=CURRENT_IP_ADDRESS&...`
+
+[DNSOMatic](https://www.dnsomatic.com/) seems to be a great service that does expose such endpoint and then integrates with Cloudflare API (and many others) though 2 negative points:
+* HTTP endpoint was deprecated and only HTTPS supported now - this router only supports HTTP
+* Requires Cloudflare Global API key instead of a more granular API token - no thank you...
+
+## Usage
+
+This worker is live at https://cfddnsupdate.fopina.workers.dev/
+
+It follows the same interface as mentioned in [dynu.com](https://www.dynu.com/DynamicDNS/IP-Update-Protocol) or [dyn.com](https://help.dyn.com/remote-access-api/perform-update/).
+
+Your router/device should be configured as
+* **Hostname**: cfddnsupdate.fopina.workers.dev
+* **HTTP port**: 80
+* **HTTPS port**: 443
+* **Path** (anything works actually): /nic/update
+* **Username**: your cloudflare zone ID
+* **Password**: your API token (**NOT** the Global API key)
+
+**Zone ID** is visible in the main page of your zone, on the right side bar under `API`.  
+**API token** is **NOT** the Global API key (avoid using that in 3rd party services). Click `Get your API token` and then create one with `Zone DNS Edit` permissions, that's all it takes. You can also restrict it to the zone you'll be using.
+
+Sample HTTP request (cURL syntax) to update IP
+```
+curl https://cfddnsupdate.fopina.workers.dev/nic/update?hostname=subdomain.example.com&myip=1.1.1.1 \
+     -u <zone ID>:<API token>
+```
+
+### Update parameters
+
+| Field | Description |
+| ----- | ----------- |
+| hostname | Hostname that you wish to update. **This is a required field.**|
+| myip | IP address to set for the update. |
+| wildcard | *currently ignored* |
+| offline | *currently ignored* |
+
+
+### Router setup
+
+In order to use this in a Thompson TG789 router, you have to `telnet` into it and execute
+
+```
+dyndns service modify name=custom server=cfddnsupdate.fopina.workers.dev port=80 updateinterval=3600
+```
+
+`updateinterval` in the example is set to update IP every hour.
+
+Then, via UI under Dynamic DNS, you add username (zone ID), password (API token) and choose the hostname to update.
+
+> PRs with instructions to other routers are very welcome
+
+## Development
+
+This project uses wrangler, included as a dependency
+
+```
+npm install
+npm run dev
+```
+
+That's all that's needed to have a local instance up and running. Then `npm run deploy` (and follow the steps) if you want to deploy your own copy.
--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,114 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Buffer } from 'node:buffer';
 
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
 }
 
+const RECORD_TYPE = "A"
+
 export default {
+	parseAuthorization(request: Request): [Response | null, string, string] {
+		// taken from https://developers.cloudflare.com/workers/examples/basic-auth/
+		const authorization = request.headers.get("Authorization");
+		if (!authorization) return [new Response("Missing authorization header", { status: 401 }), "", ""]
+
+		const [scheme, encoded] = authorization.split(" ");
+
+		// The Authorization header must start with Basic, followed by a space.
+		if (!encoded || scheme !== "Basic") {
+			return [new Response("Malformed authorization header.", {
+				status: 400,
+			}), "", ""]
+		}
+
+		const credentials = Buffer.from(encoded, "base64").toString();
+
+		// The username & password are split by the first colon.
+		//=> example: "username:password"
+		const index = credentials.indexOf(":");
+		const user = credentials.substring(0, index);
+		const pass = credentials.substring(index + 1);
+		return [null, user, pass]
+	},
+
+	async update(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		/**
+		 * Handle /nic/update?system=custom&hostname=whatever.ddns.net&myip=1.2.3.4&wildcard=OFF&offline=NO
+		 * ZONEID in basic auth username
+		 * API_TOKEN (not global key) in basic auth password
+		 * RECORD_NAME in `hostname` parameter
+		 * RECORD_CONTANT in `myip` parameter
+		 */
+		const url = new URL(request.url);
+		const recordName = url.searchParams.get("hostname");
+		const recordContent = url.searchParams.get("myip");
+		const [authParsed, zoneId, apiToken] = this.parseAuthorization(request);
+		if (authParsed !== null) return authParsed;
+		if (!recordName) return new Response("Missing hostname", { status: 400 })
+
+		const apiUrl = `https://api.cloudflare.com/client/v4/zones/${zoneId}/dns_records`;
+		const headers = {
+			// TODO: add global key option - email to be set somewhere?!
+			// 'X-Auth-Email': '',
+			// 'X-Auth-Key': 'GLOBAL KEY',
+			'Authorization': `Bearer ${apiToken}`,
+			'Content-Type': 'application/json',
+		};
+
+		const body = JSON.stringify({
+			type: RECORD_TYPE,
+			name: recordName,
+			content: recordContent,
+		});
+
+		const listResponse = await fetch(
+			apiUrl + '?' + new URLSearchParams({
+				name: recordName,
+				type: RECORD_TYPE,
+			}),
+			{
+				method: 'GET',
+				headers: headers,
+			}
+		);
+
+		if (!listResponse.ok) {
+			console.error(`Failed to list DNS records: ${recordName}, Status: ${listResponse.status}`);
+			console.error(`Message: ${await listResponse.text()}`);
+			return new Response('Failed to list DNS records', { status: 500 });
+		}
+
+		const existingRecords: any = await listResponse.json();
+		let updateUrl = apiUrl;
+		const updateInit = {
+			method: 'POST',
+			headers: headers,
+			body: body
+		}
+		if (existingRecords['result']) {
+			const recordId = existingRecords['result'][0];
+			if (recordId['content'] == recordContent) return new Response('Already up to date');
+			updateUrl = `${apiUrl}/${recordId['id']}`;
+			updateInit.method = 'PATCH';
+		}
+
+		const response = await fetch(updateUrl, updateInit);
+
+		// Handle response
+		if (response.ok) {
+			console.log(`Successfully updated DNS record: ${recordName}`);
+		} else {
+			const error = await response.text();
+			console.error(`Failed to update DNS record: ${recordName}, Status: ${response.status}`);
+			console.error(`Message: ${error}`);
+			return new Response(`Failed to update: ${error}`, {status: response.status});
+		}
+		return new Response('Done');
+	},
+
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		const url = new URL(request.url);
+		if (url.pathname.startsWith("/nic/update"))
+			return this.update(request, env, ctx);
 		return new Response('Hello World!');
 	},
 };
